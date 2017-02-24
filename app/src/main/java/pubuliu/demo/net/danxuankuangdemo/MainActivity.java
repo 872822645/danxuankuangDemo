6507e4abe4106f6a8ad568eab8bc8d0ba2e9924b
872822645 <872822645@qq.com>
2017-02-24 20:34:59 +0800

    增加了一些注释

--- app/src/main/java/pubuliu/demo/net/danxuankuangdemo/MainActivity.java
+++ app/src/main/java/pubuliu/demo/net/danxuankuangdemo/MainActivity.java
@@ -26,17 +26,18 @@ public class MainActivity extends Activity implements View.OnClickListener,Radio
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.tv_hello){
+        if(v.getId()==R.id.tv_hello){//点击控制Popupwindow生成显示与隐藏
             if(null!=suozaidiPopupWindow){
                 if(suozaidiPopupWindow.isShowing()){
-                    suozaidiPopupWindow.dismiss();
+                    suozaidiPopupWindow.dismiss();//Popupwindow消失
                 }else{
 
-                    suozaidiPopupWindow.showAsDropDown(v);
+                    suozaidiPopupWindow.showAsDropDown(v);//Popupwindow显示
                     backgroundAlpha(0.8f);
                 }
-            }else {
+            }else {//Popupwindow生成并且显示
                 suozaidiPopupWindow=new SuozaidiPopupWindow(MainActivity.this);
+                //这里设置Popwindow消失事件。恢复屏幕亮度
                 suozaidiPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
                     @Override
                     public void onDismiss() {
@@ -51,20 +52,20 @@ public class MainActivity extends Activity implements View.OnClickListener,Radio
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
-        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
-        if(group.getId()==R.id.rg_suozaidi){//所在地
-            switch(checkedId){
-                case R.id.rb_zonghe://综合排序
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());//引入图片
+        if(group.getId()==R.id.rg_suozaidi){//
+            switch(checkedId){//通过点击。设置后方对勾那个图片
+                case R.id.rb_zonghe://
                     ((RadioButton)group.getChildAt(2)).setCompoundDrawables(null,null,null,null);
                     ((RadioButton)group.getChildAt(1)).setCompoundDrawables(null,null,null,null);
                     ((RadioButton)group.getChildAt(0)).setCompoundDrawables(null,null,drawable,null);
                     break;
-                case R.id.rb_zuijin://距离最近
+                case R.id.rb_zuijin://
                     ((RadioButton)group.getChildAt(2)).setCompoundDrawables(null,null,null,null);
                     ((RadioButton)group.getChildAt(1)).setCompoundDrawables(null,null,drawable,null);
                     ((RadioButton)group.getChildAt(0)).setCompoundDrawables(null,null,null,null);
                     break;
-                case R.id.rb_xingji://星级最高
+                case R.id.rb_xingji://
                     ((RadioButton)group.getChildAt(2)).setCompoundDrawables(null,null,drawable,null);
                     ((RadioButton)group.getChildAt(1)).setCompoundDrawables(null,null,null,null);
                     ((RadioButton)group.getChildAt(0)).setCompoundDrawables(null,null,null,null);
@@ -72,6 +73,7 @@ public class MainActivity extends Activity implements View.OnClickListener,Radio
             }
         }
     }
+    /**这里调节背景亮度。出现遮罩效果，高仿dialog.单纯的Popupwindow不能很好的出现遮罩*/
     private void backgroundAlpha(float f) {
         WindowManager.LayoutParams lp =getWindow().getAttributes();
         lp.alpha = f;
